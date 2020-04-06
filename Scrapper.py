f6892ad2e142849d697bfcb38d065cb0d24502fb
Nagesh Kumar C <Nageshkumar.c@ab-inbev.com>
2020-04-06 13:59:43 +0530

    Added Code to get the regions list

--- Scrapper.py
+++ Scrapper.py
@@ -85,6 +85,9 @@ class Scrapper:
     def get_country_code_from_url(self, url):
         return re.split(". |_", os.path.basename(url))[1]
 
+    def get_region_code_from_url(self, url):
+        return re.split(". |_", os.path.basename(url))[2]
+
     def get_date_code_from_url(self, url):
         return re.split(". |_", os.path.basename(url))[0]
 
@@ -148,6 +151,17 @@ class Scrapper:
         text = text.replace("compared to baseline", "")
         return float(text.replace('%', '').strip())/100
 
+    def get_region_list(self, url="https://www.google.com/covid19/mobility/"):
+        html = self.get_content(url)
+        page = BeautifulSoup(html, 'lxml')
+        links = [tag['href'] for tag in page.select("div.region-data > a.download-link")]
+        df = pd.DataFrame(data=links, columns=['url'])
+        df['country'] = df.url.apply(self.get_country_code_from_url)
+        df['region'] = df.url.apply(self.get_region_code_from_url)
+        df['date'] = df.url.apply(self.get_date_code_from_url)
+        self.logger.info('Finished getting country list. Found {} regions.'.format(len(df)))
+        return df
+
     def get_national_data(self, url):
         self.logger.info(f'Getting natinal data for {url}')
         self.scrape_content(url)
@@ -194,3 +208,48 @@ class Scrapper:
 # Scrapper().get_county_list()
 # Scrapper().get_national_data('https://www.gstatic.com/covid19/mobility/2020-03-29_AF_Mobility_Report_en.pdf')
 Scrapper().get_sub_national_data('https://www.gstatic.com/covid19/mobility/2020-03-29_GB_Mobility_Report_en.pdf')
+# # function to get overall data from a country report
+# get_national_data < - function(url)
+# {
+#
+#     # get the report, subset to overall pages and and convert to a dataframe
+#     report_data < - pdftools:: pdf_data(url)
+# national_pages < - report_data[1:2]
+# national_data < - map_dfr(national_pages, bind_rows,.id = "page")
+#
+# # get the report file name extract the date and country
+# filename < - basename(url)
+#
+# date < - strsplit(filename, "_")[[1]][1]
+# country < - strsplit(filename, "_")[[1]][2]
+#
+# # extract the data at relevant y position
+# national_datapoints < - national_data % > %
+# filter(y == 369 | y == 486 | y == 603 |
+#        y == 62 | y == 179 | y == 296) % > %
+# mutate(
+#     entity=case_when(
+#         page == 1 & y == 369
+# ~ "retail_recr",
+# page == 1 & y == 486
+# ~ "grocery_pharm",
+# page == 1 & y == 603
+# ~ "parks",
+# page == 2 & y == 62
+# ~ "transit",
+# page == 2 & y == 179
+# ~ "workplace",
+# page == 2 & y == 296
+# ~ "residential",
+# TRUE
+# ~ NA_character_)) % > %
+# mutate(value= as.numeric(str_remove_all(text, "\\%")) / 100,
+#                  date = date,
+#                         country = country,
+#                                   location = "COUNTRY OVERALL") % > %
+# select(date, country, location, entity, value)
+#
+# # return data
+# return (national_datapoints)
+#
+# }
